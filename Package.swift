907328f199821ea5db3fba1ffd94ee7c1ba39745
Priit <priitm@gmail.com>
2021-02-24 14:07:43 -0500

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "DrawerKit",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "DrawerKit",
+      targets: ["DrawerKit"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "DrawerKit",
+      dependencies: [],
+      path: "DrawerKit/DrawerKit/",
+      exclude: ["Info.plist"],
+    cSettings: [
+        .headerSearchPath("."),
+      ]
+    ),
+  ]
+)
